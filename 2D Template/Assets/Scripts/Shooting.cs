373e1f7637a1f27dbb71af88171bae1a1cdb47dc
MUKILTEO\1720941 <jtkid2@icloud.com>
2024-12-04 10:12:48 -0800

    shoot delay, reload delay

--- 2D Template/Assets/Scripts/Shooting.cs
+++ 2D Template/Assets/Scripts/Shooting.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Shooting : MonoBehaviour
@@ -7,30 +9,67 @@ public class Shooting : MonoBehaviour
 
     public Transform FirePiont;
     public GameObject BulletPrefab;
-
+    public int BulletAmount;
+    public int MaxBulletCount;
+    public float BulletCooldown;
+    public float ReloadCooldown;
     public float bulletForce = 20f;
+    private bool canShoot = true;
+    private bool isShooting = false;
 
+
+    private void Start()
+    {
+        BulletAmount = MaxBulletCount;
+    }
     // Update is called once per frame
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            Shoot();
+            if (canShoot)
+            StartCoroutine(ShootDelay());
         }
-    }
-
-    void Shoot()
-    {
-        GameObject Bullet = Instantiate(BulletPrefab, FirePiont.position, FirePiont.rotation);
-        Rigidbody2D rb = Bullet.GetComponent<Rigidbody2D>();
-        rb.AddForce(FirePiont.up * bulletForce, ForceMode2D.Impulse);
-        StartCoroutine(ShootBullet(Bullet));
+        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine(ReloadDelay());
+        }
     }
 
     private IEnumerator ShootBullet(GameObject bullet)
     {
         yield return new WaitForSeconds(2f);
         Destroy(bullet);
+    }
+
+    private IEnumerator ShootDelay()
+    {
+        canShoot = false;
+        if (BulletAmount > 0)
+        {
+            isShooting = true;
+            BulletAmount -= 1;
+            GameObject Bullet = Instantiate(BulletPrefab, FirePiont.position, FirePiont.rotation);
+            Rigidbody2D rb = Bullet.GetComponent<Rigidbody2D>();
+            rb.AddForce(FirePiont.up * bulletForce, ForceMode2D.Impulse);
+            StartCoroutine(ShootBullet(Bullet));
+        }
+        if (BulletAmount < 0)
+            canShoot = false;
+        yield return new WaitForSeconds(BulletCooldown);
+        isShooting = false;
+        canShoot = true;
+    }
+
+    private IEnumerator ReloadDelay()
+    {
+        yield return new WaitForSeconds(ReloadCooldown);
+        if(!isShooting)
+        {
+            BulletAmount = MaxBulletCount;
+            canShoot = true;
+        }
     }
 
 }
