ccf59bfcadbb9ea82d7de43e496d97e7e127eec2
MUKILTEO\1720760 <1720760@mukilteo.wednet.edu>
2024-12-10 08:14:43 -0800

    health bar made UI compatible

--- 2D Template/Assets/healthbar.cs
+++ 2D Template/Assets/healthbar.cs
@@ -6,13 +6,14 @@ public class healthbar : MonoBehaviour
 {
     public static healthbar Instance;
 
-    float life = 100;
+
+    float life;
     public float lifemax;
     public GameObject Player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        life = 100;
     }
 
     private void Awake()
@@ -31,13 +32,14 @@ public class healthbar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.GetChild(0).localScale = new Vector3(life / lifemax,1,1);
+        transform.GetChild(0).GetComponent<RectTransform>().localScale = new Vector3(life / lifemax,1,1);
     }
     public void Hurt(float subtrahend)
     {
         life -= subtrahend;
         if (life < 0)
         {
+            life = 0;
             Destroy(Player);
         }
     }
