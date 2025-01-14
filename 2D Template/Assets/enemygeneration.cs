a8a59f4f90307da8a5b895d40135aac8842b1b2f
MUKILTEO\1722415 <1722415@mukilteo.wednet.edu>
2025-01-14 09:56:21 -0800

    no message

--- 2D Template/Assets/enemygeneration.cs
+++ 2D Template/Assets/enemygeneration.cs
@@ -113,55 +113,55 @@ public class enemygeneration : MonoBehaviour
         }
     }
 
-#if UNITY_EDITOR
-    [CustomEditor(typeof(enemygeneration))]
-    public class EnemyGenerationEditor : Editor
-    {
-        private int numberOfRounds;
+//#if UNITY_EDITOR
+//    [CustomEditor(typeof(enemygeneration))]
+//    public class EnemyGenerationEditor : Editor
+//    {
+//        private int numberOfRounds;
 
-        public override void OnInspectorGUI()
-        {
-            enemygeneration script = (enemygeneration)target;
+//        public override void OnInspectorGUI()
+//        {
+//            enemygeneration script = (enemygeneration)target;
 
-            DrawPropertiesExcluding(serializedObject, "rounds");
+//            DrawPropertiesExcluding(serializedObject, "rounds");
 
-            EditorGUILayout.Space();
-            EditorGUILayout.LabelField("Rounds Configuration", EditorStyles.boldLabel);
+//            EditorGUILayout.Space();
+//            EditorGUILayout.LabelField("Rounds Configuration", EditorStyles.boldLabel);
 
-            numberOfRounds = EditorGUILayout.IntField("Number of Rounds", numberOfRounds);
+//            numberOfRounds = EditorGUILayout.IntField("Number of Rounds", numberOfRounds);
 
-            if (GUILayout.Button("Set Number of Rounds"))
-            {
-                AdjustRoundsArray(script, numberOfRounds);
-            }
+//            if (GUILayout.Button("Set Number of Rounds"))
+//            {
+//                AdjustRoundsArray(script, numberOfRounds);
+//            }
 
-            SerializedProperty roundsProperty = serializedObject.FindProperty("rounds");
-            for (int i = 0; i < roundsProperty.arraySize; i++)
-            {
-                SerializedProperty round = roundsProperty.GetArrayElementAtIndex(i);
-                EditorGUILayout.PropertyField(round, new GUIContent($"Round {i + 1}"));
-            }
+//            SerializedProperty roundsProperty = serializedObject.FindProperty("rounds");
+//            for (int i = 0; i < roundsProperty.arraySize; i++)
+//            {
+//                SerializedProperty round = roundsProperty.GetArrayElementAtIndex(i);
+//                EditorGUILayout.PropertyField(round, new GUIContent($"Round {i + 1}"));
+//            }
 
-            serializedObject.ApplyModifiedProperties();
-        }
+//            serializedObject.ApplyModifiedProperties();
+//        }
 
-        private void AdjustRoundsArray(enemygeneration script, int newSize)
-        {
-            SerializedProperty roundsProperty = serializedObject.FindProperty("rounds");
-            roundsProperty.arraySize = newSize;
+//        private void AdjustRoundsArray(enemygeneration script, int newSize)
+//        {
+//            SerializedProperty roundsProperty = serializedObject.FindProperty("rounds");
+//            roundsProperty.arraySize = newSize;
 
-            for (int i = 0; i < newSize; i++)
-            {
-                if (roundsProperty.GetArrayElementAtIndex(i).intValue == 0)
-                {
-                    roundsProperty.GetArrayElementAtIndex(i).intValue = 0;
-                }
-            }
+//            for (int i = 0; i < newSize; i++)
+//            {
+//                if (roundsProperty.GetArrayElementAtIndex(i).intValue == 0)
+//                {
+//                    roundsProperty.GetArrayElementAtIndex(i).intValue = 0;
+//                }
+//            }
 
-            serializedObject.ApplyModifiedProperties();
-        }
-    }
-#endif
+//            serializedObject.ApplyModifiedProperties();
+//        }
+//    }
+//#endif
 }
 //2
 
